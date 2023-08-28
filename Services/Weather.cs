f918a4c09b6e81540d8c4a36bb3198c579feea2c
arsalan.khan <arsalan.khan@seekinvest.com>
2023-08-29 03:46:04 +0500

    Correction error

--- Services/Weather.cs
+++ Services/Weather.cs
@@ -4,9 +4,9 @@ namespace WeatherMVCAPP.Services
 {
     public class Weather
     {
-        public string City { get; set; }
-        public string Temperature { get; set; }
-        public string Description { get; set; }
+        public string? City { get; set; }
+        public string? Temperature { get; set; }
+        public string? Description { get; set; }
     }
 
     public class WeatherApiResponse
